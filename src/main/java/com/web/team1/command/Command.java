ddd02f5c977095a6052c21e2d20f43fc97a22362
dnflwhr1004 <dnflwhr1004@gmail.com>
2019-10-10 17:54:31 +0900

    20191010 "17:54"

--- src/main/java/com/web/team1/command/Command.java
+++ src/main/java/com/web/team1/command/Command.java
@@ -15,7 +15,7 @@ public class Command implements Order {
 
 		public void execute() {
 			this.view = String.format(Constants.DOUBLE_PATH,"facade", "main");
-			System.out.println("커멘드뷰" + view);
+			System.out.println("커멘드뷰..." + view);
 	}
 // 커멘드으
 }
