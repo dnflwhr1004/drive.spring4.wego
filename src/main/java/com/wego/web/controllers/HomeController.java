5324f7eb3d0448fcaaeac0819a4c758cb3bd9f45
dnflwhr1004 <dnflwhr1004@gmail.com>
2019-10-16 18:16:55 +0900

    20191016 "18:16"

--- src/main/java/com/wego/web/controllers/HomeController.java
+++ src/main/java/com/wego/web/controllers/HomeController.java
@@ -2,25 +2,21 @@ package com.wego.web.controllers;
 import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.wego.web.serviceimpl.AdminServiceImpl;
-import com.wego.web.services.AdminService;
 
 @Controller
 public class HomeController {
 	
 	private static final Logger Logger = LoggerFactory.getLogger(HomeController.class);
-	@Autowired AdminServiceImpl adminService;
+	
 	
 	@GetMapping("/")
 	public String home(Locale locale, Model model) {
 		Logger.info("Welcome  {}.", "HomeController");
-		int count = adminService.findTheNumberOfAdmins();
-		model.addAttribute("count", count );
+		
 		return "home";
 	}
 	
